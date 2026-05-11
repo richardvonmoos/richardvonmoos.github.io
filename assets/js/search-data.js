8fe2244b8472b8662f3236d91f78c72b2cf5c6cd
richardvonmoos <rw.vonmoos@gmail.com>
2026-05-11 18:11:34 +0000

    Deploying to gh-pages from @ richardvonmoos/richardvonmoos.github.io@c195effdbd4bca73ca5c483db3aec595fc95e8a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-started-my-doctorate-at-the-aidos-lab",
+          title: 'I started my doctorate at the AIDOS lab!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
