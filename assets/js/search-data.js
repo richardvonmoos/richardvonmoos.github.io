6953d97199ae907adc2014672638afc9da4e1f98
richardvonmoos <rw.vonmoos@gmail.com>
2026-05-11 18:32:55 +0000

    Deploying to gh-pages from @ richardvonmoos/richardvonmoos.github.io@890ac2638ba72604211b3127fd12869dba864d94 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,10 +391,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-started-my-doctorate-at-the-aidos-lab",
+            },},{id: "news-i-started-my-doctorate-at-the-aidos-lab",
           title: 'I started my doctorate at the AIDOS lab!',
           description: "",
           section: "News",},{id: "projects-project-1",
@@ -453,46 +450,18 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%69%63%68%61%72%64.%76%6F%6E%6D%6F%6F%73@%75%6E%69%66%72.%63%68", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/richardvonmoos", "_blank");
         },
       },{
       id: 'light-theme',
