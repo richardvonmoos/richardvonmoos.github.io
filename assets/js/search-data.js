cf9c1eafc5b795970560ff97343c995e74cef4fe
richardvonmoos <rw.vonmoos@gmail.com>
2026-05-12 14:03:11 +0000

    Deploying to gh-pages from @ richardvonmoos/richardvonmoos.github.io@b7d739336bc2274eba0016166cff7cf4174e3a3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "My CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
